6e27445d5c83bbaf4e4073cea6dd21ae106ba170
md8119 <139487467+md8119@users.noreply.github.com>
2023-10-19 21:46:05 +0100

    Grid created

--- board.py
+++ board.py
@@ -1,8 +1,18 @@
 import pygame
 import sys
+import math
 
 # Initialize Pygame
 pygame.init()
+
+
+
+
+# CONSTANTS
+HEX_SIZE = 30
+ROWS, COLUMNS = 60, 50  # The size of the board
+HEX_COLOR = (104, 126, 255)
+FRAME_COLOR = (244, 206, 20)
 
 # Get the user's current screen resolution
 user_screen_info = pygame.display.Info()
@@ -12,13 +22,24 @@ user_screen_height = user_screen_info.current_h
 # Create the size of the game window with the user's screen re4solution
 WINDOW_SIZE = (user_screen_width, user_screen_height)
 
-# OTHER CONSTANTS #
-BG_COLOR = ((53, 82, 255))  # Background color
-
 # Create the game window
 screen = pygame.display.set_mode(WINDOW_SIZE, pygame.FULLSCREEN)
 pygame.display.set_caption("Hexagonal Board")
 
+# Define hexagon drawing function
+def draw_hexagon(x, y):
+    hexagon_points = []
+    for i in range(6):
+        angle = 2 * math.pi / 6 * i
+        px = x + HEX_SIZE * math.cos(angle)
+        py = y + HEX_SIZE * math.sin(angle)
+        hexagon_points.append((px, py))
+
+    pygame.draw.polygon(screen, HEX_COLOR, hexagon_points)
+    pygame.draw.polygon(screen, FRAME_COLOR, hexagon_points, 2)
+
+#######################################
+# MAIN LOOP
 running = True
 while running:
     for event in pygame.event.get():
@@ -26,7 +47,25 @@ while running:
             running = False
 
     # Clear the screen
-    screen.fill(BG_COLOR)
+    screen.fill((53, 82, 255))
+
+    # Draw the hexagonal grid
+    for row in range(ROWS):
+        for col in range(COLUMNS):
+            x = col * (3/2 * HEX_SIZE)
+            y = row * (HEX_SIZE * math.sqrt(3))
+            if col % 2 == 1:
+                y += (HEX_SIZE * math.sqrt(3)) / 2
+            draw_hexagon(x, y)
+
+
+
+
+
+
+
+
+
 
     # Update the display
     pygame.display.flip()
