af8ca11acfe790ce9385aa2dbd1c4b09bd88b8e3
md8119 <139487467+md8119@users.noreply.github.com>
2023-10-20 17:36:20 +0100

    Added hexagons coord + opacity control

--- board.py
+++ board.py
@@ -1,18 +1,22 @@
 import pygame
 import sys
 import math
+import string
 
 # Initialize Pygame
 pygame.init()
-
-
-
+###################
 
 # CONSTANTS
 HEX_SIZE = 30
 ROWS, COLUMNS = 60, 50  # The size of the board
 HEX_COLOR = (104, 126, 255)
 FRAME_COLOR = (244, 206, 20)
+COORDINATES_OPACITY = 150   # Adjust this value for opacity (0-255)
+
+# VARIABLES
+field_coordinates = []
+letters = [char for char in string.ascii_uppercase]
 
 # Get the user's current screen resolution
 user_screen_info = pygame.display.Info()
@@ -26,6 +30,16 @@ WINDOW_SIZE = (user_screen_width, user_screen_height)
 screen = pygame.display.set_mode(WINDOW_SIZE, pygame.FULLSCREEN)
 pygame.display.set_caption("Hexagonal Board")
 
+def get_letter(index):
+    try:
+        return letters[index]
+    except IndexError:
+        # Extend the list to include double letters
+        while index >= len(letters):
+            new_letter = get_letter(index // 26 - 1) + get_letter(index % 26)
+            letters.append(new_letter)
+        return letters[index]
+
 # Define hexagon drawing function
 def draw_hexagon(x, y):
     hexagon_points = []
@@ -37,6 +51,14 @@ def draw_hexagon(x, y):
 
     pygame.draw.polygon(screen, HEX_COLOR, hexagon_points)
     pygame.draw.polygon(screen, FRAME_COLOR, hexagon_points, 2)
+
+    # Display the coordinates on the hexagon
+    col_letter = get_letter(col)
+    hex_font = pygame.font.Font(None, 24)
+    text = hex_font.render(f'{col_letter}{col}', True, (255, 255, 255))
+    text.set_alpha(COORDINATES_OPACITY)
+    text_rec = text.get_rect(center=(x, y))
+    screen.blit(text, text_rec)
 
 #######################################
 # MAIN LOOP
@@ -57,6 +79,7 @@ while running:
             if col % 2 == 1:
                 y += (HEX_SIZE * math.sqrt(3)) / 2
             draw_hexagon(x, y)
+            field_coordinates.append((x, y))
 
 
 
